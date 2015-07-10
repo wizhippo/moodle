6da8077aff883c3cff9424297a8510fb87630a8b
Douglas Hammond <wizhippo@gmail.com>
2015-07-09 23:11:15 -0400

    Add auth_jwt support

---
+++ auth/jwt/settings.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Admin settings and defaults.
+ *
+ * @package auth_jwt
+ * @copyright  2017 Douglas hammond
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+    // Introductory explanation.
+    $settings->add(new admin_setting_heading('auth_jwt/pluginname', '',
+        new lang_string('auth_jwtdescription', 'auth_jwt')));
+
+    // Url.
+    $settings->add(new admin_setting_configtext('auth_jwt/url', get_string('auth_jwturl', 'auth_jwt'),
+        get_string('auth_jwturl', 'auth_jwt'), 'https://localhost/auth', PARAM_RAW));
+
+    // Key.
+    $settings->add(new admin_setting_configtext('auth_jwt/key', get_string('auth_jwtkey', 'auth_jwt'),
+        get_string('auth_jwtkey', 'auth_jwt'), 'secretkey', PARAM_RAW));
+
+    // Display locking / mapping of profile fields.
+    $authplugin = get_auth_plugin('jwt');
+    display_auth_lock_options($settings, $authplugin->authtype, $authplugin->userfields,
+        get_string('auth_fieldlocks_help', 'auth'), false, false);
+}
